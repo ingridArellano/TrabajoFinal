92ae8718ac420c987a89143739159d55965f96cc
Ingrid Arellano <ingrid10arellano@gmail.com>
2019-06-04 16:41:21 -0500

    Actualizado de contacto.php

--- contacto.php
+++ contacto.php
@@ -6,6 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Contacto</title>
     <link rel="stylesheet" href="estilos/hojaContacto.css">
+    <style> 
+        html { 
+        background: url(imagenes/animalesDomesticos.jpg) no-repeat center fixed; 
+        background-size: cover;
+        }
+    </style>
 </head>
 <body>
     <div>
@@ -17,32 +23,43 @@
 
             <div class="hijo">
                 <label>Nombres:</label>
-                <input type="text" name="n">
+                <input class="inputt" type="text" name="n">
             </div>
             
             <div class="hijo">
                 <label>Apellidos:</label>
-                <input type="text" name="a">
+                <input class="inputt" type="text" name="a">
             </div>
 
             <div class="hijo">
-                <label>Correo Electrónico:</label>
-                <input type="text" name="c">
+                <label>CorreoElectrónico:</label>
+                <input class="inputt" type="text" name="c">
             </div>
             
             <div class="hijo">
                 <label>Asunto:</label>
-                <input type="text" name="as">
+                <input class="inputt" type="text" name="as">
             </div>
 
             <div class="hijo">
-                <label>Mensaje</label>
+                <label>Mensaje:</label>
                 <input class="inputt" type="text" name="m">
             </div>
 
             <div class="hijo">
                 <input type="checkbox" id="tos" name="tos" value="tos">
-                <label for="tos">Acepto los términos y condiciones </label>
+                <label for="tos">En cumplimiento de la ley de protección de Datos Personales, 
+                                 por medio del presente documento solicitamos su autorización 
+                                 para que AdoptMe recolecte, almacene y use sus datos personales 
+                                 registrados en el presente formulario. Sus datos personales se 
+                                 almacenarán de manera segura, habiendo tomado todas las medidas 
+                                 de precaución para proteger su información contra adulteraciones, 
+                                 pérdidas, consultas, uso o acceso no autorizado o fraudulento. 
+                                 Esto con el objetivo de que su información esté protegida, por 
+                                 medio de la presente aceptación, Usted declara expresamente que 
+                                 la finalidad de la utilización por AdoptMe de sus datos personales, 
+                                 le ha sido plenamente informada y autoriza de modo expreso la
+                                 manipulación de sus datos.</label>
             </div>
 
             <button class="boton">Enviar</button>
