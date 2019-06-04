685a8e4606ed10e09dbf86dd9d057200cef4a253
Maricielo Flores <maricielofm7@gmail.com>
2019-06-04 16:47:12 -0500

    creado de cabecera con su hoja de estilo

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="estilos/hojaIndex.css">
+    <title>PAGINA PRINCIPAL</title>
 </head>
+
 <body>
-    AdoptMe
+    <?php include 'cabecera.php'; ?>
+
+    <div class="pagina">
+
+        <a>Pagina Principal</a>
+        
+    </div>
+
+
+    <?php include 'pie.php'; ?>
 </body>
+
 </html>
---
+++ pie.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="estilos/hojaIndex.css">
+    <title>PAGINA PRINCIPAL</title>
+</head>
+
+<body>
+<div class="padre">
+        <img src="https://d500.epimg.net/cincodias/imagenes/2018/11/13/lifestyle/1542113135_776401_1542116070_noticia_normal.jpg">
+        <nav class="navegacionf">
+            <ul class="lista">
+                <li><a href="index.php">AdoptMe</a></li>
+                <li><a href="adoptar.php">Adoptar</a></li>
+                <li><a href="doyenadopcion.php">Doy en Adopción</a></li>
+                <li><a href="campaña.php">Campaña</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="contacto.php">Contacto</a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+
+</html>
