e2e1d44868c887f6eaa01805deaab14abdf80a36
Maricielo Flores <maricielofm7@gmail.com>
2019-06-11 16:01:54 -0500

    actualizacion

--- pie.php
+++ pie.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="estilos/hojaIndex.css">
-    <title>PAGINA PRINCIPAL</title>
+    <link rel="stylesheet" href="estiloIndex.css">
+    <title>Document</title>
 </head>
-
 <body>
-<div class="padre">
-        <img src="https://d500.epimg.net/cincodias/imagenes/2018/11/13/lifestyle/1542113135_776401_1542116070_noticia_normal.jpg">
-        <nav class="navegacionf">
-            <ul class="lista">
-                <li><a href="index.php">AdoptMe</a></li>
-                <li><a href="adoptar.php">Adoptar</a></li>
-                <li><a href="doyenadopcion.php">Doy en Adopción</a></li>
-                <li><a href="campaña.php">Campaña</a></li>
-                <li><a href="blog.php">Blog</a></li>
-                <li><a href="contacto.php">Contacto</a></li>
-            </ul>
-        </nav>
-    </div>
-</body>
+<footer>
+<div class="navegacion1">
+        <div class="imagen">
+            <img src="https://sites.google.com/site/webquestanimalesdom/_/rsrc/1472873888662/home/10780695-ilustracion-de-tres-animales-domesticos%20png.png">
+        </div>
+        <ul>
+            <li><a href="index.php">AdoptMe</a></li>
+            <li><a href="adoptar.php">Adoptar</a></li>
+            <li><a href="doyenadopcion.php">Doy en Adopción</a></li>
+            <li><a href="campaña.php">Campaña</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+        </ul>
 
+    </div>
+</footer>
+</body>
 </html>
---
+++ principal.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PAGINA PRINCIPAL</title>
+    <link rel="stylesheet" href="estiloIndex.css">
+</head>
+
+<body>
+    <?php include 'cabecera.php'; ?>
+
+    <div class="slider">
+        <ul>
+            <li>
+                <img src="http://dominicushoeve.com/wp-content/uploads/ktz/latest-high-resolution-wallpaper-1920x1080-70558-pictures-high-resolution-wallpaper-30whtvl34j4r12m8b0c1sa.jpg" alt="">
+            </li>
+            <li>
+                <img src="http://youghaltennisclub.ie/wp-content/uploads/2014/06/Tennis-Wallpaper-High-Definition-700x300.jpg" alt="">
+            </li>
+            <li>
+                <img src="http://welltechnically.com/wp-content/uploads/2013/08/android-wallpapers-700x300.jpg" alt="">
+            </li>
+            <li>
+                <img src="http://welltechnically.com/wp-content/uploads/2013/09/android-widescreen-wallpaper-14165-hd-wallpapers-700x300.jpg" alt="">
+            </li>
+        </ul>
+    </div>
+
+    <div class="padre">
+
+        <div class="adopta">
+            <img class="partedos" src="https://img.blogs.es/anexom/wp-content/uploads/2018/07/23815272238_77f1b41a56_k.jpg">
+            <div class="boton">
+                <a class="tipo" href="adopta.php"><input type="button" value="ADOPTA"></a>
+                <p>¿Buscas una mascota? Elige una y cambia tu vida y la de él.</p>
+            </div>
+        </div>
+
+        <div class="ayuda">
+            <img class="partedos" src="https://misanimales.com/wp-content/uploads/2018/09/beneficios-para-una-persona-soltera-de-tener-una-mascota.jpg">
+            <div class="boton">
+                <a class="tipo" href="campaña.php"><input type="button" value="AYUDA"></a>
+                <p>¿Amas a las mascotas? Unete y contribuye a ser uno de los programas mas grandes de voluntariado</p>
+            </div>
+        </div>
+
+        <div class="contribuye">
+            <img class="partedos" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpCAjTNSKMBxtEZhwILHkZ0tzRoS8yck57yeGu_UlXub_3ocx1CQ">
+            <div class="boton">
+                <a class="tipo" href="blog.php"><input type="button" value="CONTRIBUYE"></a>
+                <p>Estamos en constante actualización por ello denos tu opinión y generarás mas valor tanto para ti como para los de ADOPTME.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="definicion">
+        <h1>¿QUÉ ES UN ADOPTME?</h1>
+        <div class="separacion">
+            <div class="hijos">
+            <img class="circulo" src="https://st4.depositphotos.com/1001599/20321/v/1600/depositphotos_203218360-stock-illustration-pet-rescue-hand-drawn-outline.jpg" >
+            <br> <br> <br> <br>
+            <p style="color: blue; font-size:20px; font-weight: bold">Es una mascota rescatada</p>
+            <br>
+            Un ADOPTME es una mascota que ha sido rescatada del abandono o una mala crianza. Todos ellos han pasado por un proceso de socialización en los albergues, lo que garantiza su recuperación.
+            </div>
+
+            <div class="hijos">
+            <img class="circulo" src="https://st3.depositphotos.com/7358038/12911/v/950/depositphotos_129118996-stock-illustration-dog-collar-icon.jpg" >
+            <br> <br> <br> <br>
+            <p style="color: blue; font-size:20px; font-weight: bold">Es una mascota segura</p>
+            <br> 
+            Todos los ADOPTMEs están desparasitados, vacunados y esterilizados. Pasan pruebas psicológicas y clínicas.
+            </div>
+
+            <div class="hijos">
+            <img class="hijito" src="https://alunaaw.org/wp-content/uploads/2019/02/Mascotas.png" >
+            </div>
+
+            <div class="hijos">
+            <img class="circulo" src="http://media.gettyimages.com/vectors/lost-dog-sign-sketch-icon-vector-id834906334?s=170667a&w=1007" >
+            <br> <br> <br> <br> 
+            <p style="color: blue; font-size:20px; font-weight: bold">Es una mascota única</p>
+            <br> 
+            Todos los ADOPTMEs son sobrevivientes, son héroes con historias y experiencias que los hacen únicos.
+            </div>
+
+            <div class="hijos">
+            <img class="circulo" src="https://comps.canstockphoto.es/labrador-perro-rescate-mascota-eps-vectorial_csp48108182.jpg" >
+            <br> <br> <br> <br> 
+            <p style="color: blue; font-size:20px; font-weight: bold">Es una mascota noble</p>
+            <br>
+            Un ADOPTME agradecerá siempre poder compartir su vida a tu lado. Será un amigo fiel que te acompañará y alegrará tus días.
+            </div>
+        </div>
+    </div>
+
+    <div class="convence">
+        <h1>¿AÚN NO TE CONVENCE ADOPTAR?</h1>
+        <div class="peque">
+            <div class="mini">
+                <img src="http://web.optimusveterinaria.com/ficheros/que_es/4.png">
+            </div>
+            <div class="mini">
+                <ul>
+                    <li>hola ADOPTME</li>
+                    <li>hola ADOPTME</li>
+                    <li>hola ADOPTME</li>
+                    <li>hola ADOPTME</li>
+                    <li>hola ADOPTME</li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    
+    <?php include 'pie.php'; ?>
+
+</body>
+
+</html>
