d9f90f1c6735df15f0231f65d64ee0dcca7f59f3
Sharon <sharonc.quispec@gmail.com>
2019-06-11 14:04:56 -0500

    seccion blog

---
+++ blog.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" a href="estilos_blog.css">
+
+</head>
+<body>
+    <div class="bigBox">
+        <div class="box">
+            <div class="imgBox"> 
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDRLnTXLMYOZq54hSlmtjH_7FBVuM8RZWb10zBrMJhZzYmxRnN" alt="">
+            </div>
+            <p>Lorem, ipsum dolor sit amet 
+                consectetur adipisicing elit. Saepe 
+                pariatur consequuntur atque fuga eaque
+                 eius dolores natus nam accusantium iusto placeat obcaecati, ut nostrum blanditiis aliquid, quisquam eos veritatis fugiat?</p>
+            <h4>Someone Famous<br><span>Cara Develigne</span></h4>
+
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDRLnTXLMYOZq54hSlmtjH_7FBVuM8RZWb10zBrMJhZzYmxRnN" alt="">
+
+            </div>
+            <p>Lorem, ipsum dolor sit amet 
+                consectetur adipisicing elit. Saepe 
+                pariatur consequuntur atque fuga eaque
+                 eius dolores natus nam accusantium iusto placeat obcaecati, ut nostrum blanditiis aliquid, quisquam eos veritatis fugiat?</p>
+            <h4>Someone Famous<br><span>Cara Develigne</span></h4>
+
+        </div>
+
+
+    </div>
+    
+</body>
+</html>
--- perfil.php
+++ perfil.php
@@ -11,48 +11,74 @@
 <?php include 'cabecera.php'?>
 <h1>AdoptMe</h1>
     
-    <table class="tablita">
+    
+    <div class="cuadrito">
+       <div class="seccion_arriba">
+    
+            <img id="imagen" src="http://cdn.lavozdesanjusto.com.ar/Imagenes/1200Image1534794eb6f149ba9ed94d6fa86fd45b.jpg" alt="">
+                <div class="nav_imagen">
 
-    <tr>
-        <th>Pefil AdoptMe</th>
-        <th>Nombre</th>
-        <th>Sexo</th>
-        <th>Tamaño</th>
-        <th>Peso</th>
-        <th>Edad</th>
-        <th>Nivel de actividad</th>
-    </tr>
-    
-    
-        <tr>
-            <td>
-            <img class="imagen" src="http://cdn.lavozdesanjusto.com.ar/Imagenes/1200Image1534794eb6f149ba9ed94d6fa86fd45b.jpg" alt="">
-            </td>
-            <td>
+                <img onclick="cambiar_imagen(this)" src="http://cdn.lavozdesanjusto.com.ar/Imagenes/1200Image1534794eb6f149ba9ed94d6fa86fd45b.jpg" alt="">
+                <img onclick="cambiar_imagen(this)" src="https://www.royalcanin.es/wp-content/uploads/2016/03/comportamiento-perro.jpg" alt="">
+                <img onclick="cambiar_imagen(this)" src="https://seguros.elcorteingles.es/admin/assets/uploads/files/images/thumbnails_l/EL-PERRO-NO-BEBE-AGUA.jpg" alt="">
+
+            </div>
+
+            <div class="descripcion">
+        
             <?php echo $fila["nombre_masc"] ?>
-            <td>
-            <?php echo $fila["sexo_masc"] ?>
-            </td>
-            <td>
-            <?php echo $fila["tamaño_masc"] ?>
-            </td>
-            <td>
-            <?php echo $fila["peso_masc"] ?>
-            </td>
-            <td>
-            <?php echo $fila["edad_masc"] ?>
-            <td>
-            <?php echo $fila["nivel_act"] ?>
-            </td>
             
+            </div>
 
-        </tr>
-    
-    </table>
-    <div class="centrar">
-    <a class="aceptar" href="inscribete.php?id=<?php echo $registroMascota["id_masc"]?>">AdoptMe ya!</a>
+        <div class="seccion_abajo">
+            <table class="tablita">
+            <div class="name"><?php echo $fila["nombre_masc"] ?>
+            </div>
+        
+            <tr>
+                <th>Sexo</th>
+                <th>Tamaño</th>
+                <th>Peso</th>
+                <th>Edad</th>
+                <th>Nivel de actividad</th>
+            </tr>
+
+            <tr>
+                <td>
+                <?php echo $fila["sexo_masc"] ?>
+                </td>
+                <td>
+                <?php echo $fila["tamaño_masc"] ?>
+                </td>
+                <td>
+                <?php echo $fila["peso_masc"] ?>
+                </td>
+                <td>
+                <?php echo $fila["edad_masc"] ?>
+                <td>
+                <?php echo $fila["nivel_act"] ?>
+                </td>
+            
+            </tr>
+            </table>
+            
+            <a class="btn" href="inscribete.php?id=<?php echo $registroMascota["id_masc"]?>">AdoptMe ya!</a>
+                
+        </div>
     </div>
-    <?php include 'pie.php'?>
+        
+    
+
+    <script type="text/javascript">
+        var contenedor=document.getElementById("imagen");
+         function cambiar_imagen(image){
+             contenedor.src=image.src;
+}
+
+    
+    </script>
+
+<?php include 'pie.php'?>
 </body>
 
 </html>
