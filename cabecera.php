d02c1974c50f6f0eea0762a15cc8c070657f79d3
Maricielo Flores <maricielofm7@gmail.com>
2019-06-17 18:48:48 -0500

    cabecera y pie actualizados

--- cabecera.php
+++ cabecera.php
@@ -5,25 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="estiloIndex.css">
+    <link rel="stylesheet" href="estilo-header-footer.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div class="navegacion">
-        <div class="imagen">
-            <img src="https://sites.google.com/site/webquestanimalesdom/_/rsrc/1472873888662/home/10780695-ilustracion-de-tres-animales-domesticos%20png.png">
+    <header>
+        <div class="contenedor">
+            <input type="checkbox" id="boton">
+            <label  for="boton"><img src="main.png"></label>
+            <div class="hola">
+                <img src="https://sites.google.com/site/webquestanimalesdom/_/rsrc/1472873888662/home/10780695-ilustracion-de-tres-animales-domesticos%20png.png">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php">AdoptMe</a></li>
+                    <li><a href="adoptar.php">Adoptar</a></li>
+                    <li><a href="doyenadopcion.php">Doy en Adopción</a></li>
+                    <li><a href="campañas.html">Campaña</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="contacto.php">Contacto</a></li>
+                </ul>
+            </nav>
         </div>
-        <ul>
-            <li><a href="index.php">AdoptMe</a></li>
-            <li><a href="adoptar.php">Adoptar</a></li>
-            <li><a href="doyenadopcion.php">Doy en Adopción</a></li>
-            <li><a href="campañas.html">Campaña</a></li>
-            <li><a href="blog.php">Blog</a></li>
-            <li><a href="contacto.php">Contacto</a></li>
-        </ul>
-
-    </div>
+    </header>
 </body>
-
 </html>
--- pie.php
+++ pie.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="estiloIndex.css">
-    <title>Document</title>
-</head>
-<body>
-<footer>
-<div class="navegacion1">
-        <div class="imagen">
-            <img src="https://sites.google.com/site/webquestanimalesdom/_/rsrc/1472873888662/home/10780695-ilustracion-de-tres-animales-domesticos%20png.png">
-        </div>
-        <ul>
-            <li><a href="index.php">AdoptMe</a></li>
-            <li><a href="adoptar.php">Adoptar</a></li>
-            <li><a href="doyenadopcion.php">Doy en Adopción</a></li>
-            <li><a href="campañas.html">Campaña</a></li>
-            <li><a href="blog.php">Blog</a></li>
-            <li><a href="contacto.php">Contacto</a></li>
-        </ul>
 
-    </div>
-</footer>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="estilo-header.footer.css">
+	<title>Document</title>
+</head>
+
+<body>
+	<footer class="main-footer">
+		<div class="foo">
+			<img src="https://sites.google.com/site/webquestanimalesdom/_/rsrc/1472873888662/home/10780695-ilustracion-de-tres-animales-domesticos%20png.png">
+		</div>
+
+		<div class="main">
+			<ul>
+				<li><a href="index.php">AdoptMe</a></li>
+				<li><a href="adoptar.php">Adoptar</a></li>
+				<li><a href="doyenadopcion.php">Doy en Adopción</a></li>
+				<li><a href="campañas.html">Campaña</a></li>
+				<li><a href="blog.php">Blog</a></li>
+				<li><a href="contacto.php">Contacto</a></li>
+			</ul>
+		</div>
+
+	</footer>
 </body>
 </html>
